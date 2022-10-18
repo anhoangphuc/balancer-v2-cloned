0ef7d8471c2651d0e6ad2e5bf12c954cbf55a7cf
hoangphuc <hoangphucnb97@gmail.com>
2022-10-18 17:42:01 +0700

    Add IPoolSwapStructs

---
+++ contracts/interfaces/vault/IPoolSwapStructs.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.12;
+
+import "./IVault.sol";
+
+interface IPoolSwapStructs {
+    struct SwapRequest {
+        IVault.SwapKind kind;
+        IERC20 tokenIn;
+        IERC20 tokenOut;
+        uint256 amount;
+        bytes32 poolId;
+        uint256 lastChangeBlock;
+        address from;
+        address to;
+        bytes userData;
+    }    
+}
