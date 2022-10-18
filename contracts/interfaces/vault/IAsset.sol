1afb4feeb797f4356674b05974770145d9ae654a
hoangphuc <hoangphucnb97@gmail.com>
2022-10-18 16:15:51 +0700

    Add IAsset interface

---
+++ contracts/interfaces/vault/IAsset.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.12;
+
+/**
+ * @dev This is an empty interface used to represent either ERC20-conforming token contracts or ETH
+ */
+interface IAsset {
+}
