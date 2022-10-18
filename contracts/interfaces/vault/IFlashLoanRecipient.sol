3dd4fc7de6f7c2c988c4e66346ca7ba9e9588b04
hoangphuc <hoangphucnb97@gmail.com>
2022-10-18 16:23:48 +0700

    Add IFlashLoanRecipient interface

---
+++ contracts/interfaces/vault/IFlashLoanRecipient.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: UNLICENSED 
+pragma solidity 0.8.12;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IFlashLoanRecipient {
+    /**
+     * @dev When `flashLoan` is called on the Vault, it invokes the `receiverFlashLoan` hook on the recipient
+     */
+    function receiveFlashLoan(
+        IERC20[] memory tokens,
+        uint256[] memory amounts,
+        uint256[] memory feeAmounts,
+        bytes memory userData
+    ) external;
+}
